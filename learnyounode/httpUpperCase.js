85e05fe17f9ac45b02331cd1ec242cfb4fb91ef2
aditiverma1009 <aditiverma1009@gmail.com>
2018-02-11 21:48:21 +0530

    node : http Upper Case

---
+++ learnyounode/httpUpperCase.js
@@ -0,0 +1,9 @@
+const http = require('http');
+const map = require('through2-map');
+
+
+const server = http.createServer((request, response) => {
+  request.pipe(map(chunk => chunk.toString().toUpperCase())).pipe(response);
+});
+server.listen(Number(process.argv[2]));
+
